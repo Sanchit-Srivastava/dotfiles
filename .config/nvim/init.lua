baadb52dcc0a1ae0ba00cad75a48af4bdcd37ca0
Sanchit Srivastava <72091210+Sanchit-Srivastava@users.noreply.github.com>
2024-12-01 16:44:27 -0500

    added neovim config folder

---
+++ .config/nvim/init.lua
@@ -0,0 +1,21 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("vim-options")
+require("remaps")
+require("lazy").setup("plugins")
+
+-- Set transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" }) -- Ensure non-current windows are transparent too
---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,69 @@
+--Leader is space key
+vim.g.mapleader = " "
+
+--enable mouse
+vim.opt.mouse = "a"
+
+--Line numbering : using hybrid for now
+vim.opt.nu = true
+vim.opt.rnu = true
+
+-- Indenting
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+--backup and undo
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = "~/.config/nvim/.undo//"
+vim.opt.undofile = true
+
+--vim search options
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+--colourse
+vim.opt.termguicolors = true
+
+--scrollingc
+vim.opt.scrolloff = 8
+
+--random
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+-- vim.opt.colorcolumn = "80"
+vim.opt.splitright = true
+
+-- Open help files in a vertical split
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "help",
+	callback = function()
+		local width = math.floor(vim.o.columns * 0.5) -- 50% of total width
+		vim.cmd("wincmd L | vertical resize " .. width)
+	end,
+})
+
+-- Open git in a vertical split
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "fugitive",
+	callback = function()
+		local width = math.floor(vim.o.columns * 0.3) -- 30% of total width
+		vim.cmd("wincmd L | vertical resize " .. width)
+	end,
+})
+
+--Vimtex options
+vim.cmd("set encoding=utf-8")
+vim.cmd("filetype plugin on")
+vim.g.vimtex_quickfix_mode = 0
+vim.g.vimtex_log_verbose = 0
+
+--Python formatting with global black--
+vim.keymap.set("n", "<leader>bf", ":silent !black <CR>")
+
+-- setting transparency
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })

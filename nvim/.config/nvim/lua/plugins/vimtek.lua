a2cd744e6219741f8d891659ec35e142da271c9a
Sanchit Srivastava <72091210+Sanchit-Srivastava@users.noreply.github.com>
2024-12-09 02:04:58 -0500

    modified vimtex config

--- nvim/.config/nvim/lua/plugins/vimtek.lua
+++ nvim/.config/nvim/lua/plugins/vimtek.lua
@@ -1,9 +1,27 @@
 return {
-  "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
-    -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
-  end
- }
+	"lervag/vimtex",
+	lazy = false, -- we don't want to lazy load VimTeX
+	-- tag = "v2.15", -- uncomment to pin to a specific release
+	init = function()
+		-- VimTeX configuration goes here, e.g.
+		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_compiler_latexmk = {
+			build_dir = "",
+			options = {
+				"-pdf",
+				"-interaction=nonstopmode",
+				"-synctex=1",
+				"-lualatex",
+			},
+		}
+		-- Define the LaTeX engine (optional)
+		vim.g.vimtex_compiler_latexmk_engines = {
+			["_"] = "-pdf",
+			["xelatex"] = "-xelatex",
+			["lualatex"] = "-lualatex",
+		}
+
+		-- Automatically detect the main .tex file (optional)
+		vim.g.vimtex_main_file = "main.tex"
+	end,
+}
